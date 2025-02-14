ad77545e9b6788df9dccb1c1018863c9995fc882
Shashwat365 <skshasha121312@gmail.com>
2025-02-14 16:24:45 +0530

    sum

---
+++ Projects/sum of argument.js
@@ -0,0 +1,17 @@
+var Codingal=['apple' , 'banana' , 'chiku' , 'mango']
+console.log(Codingal[3])
+function add (a,b){ 
+    return a+b;
+}
+   
+function average(a,b){
+    return add(a,b)/2
+}
+let result= average(10,20);
+console.log(result)
+
+var fruit=['Shashwat' , 'Rudransh' , 'Kabeer' , 'sarthak'];
+console.log(fruit.join("👌"));
+console.log(fruit);
+fruit.pop();
+console.log(fruit);
