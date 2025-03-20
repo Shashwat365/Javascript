45218f5127c788b7c3bde1ebd3d07acf4e62433d
Shashwat365 <skshasha121312@gmail.com>
2025-03-20 20:53:44 +0530

    map

---
+++ Projects/map.js
@@ -0,0 +1,12 @@
+function myFunction(){
+    var x=54;
+    var y=21;
+    var a=eval("x + y");
+    var b=eval("2 * 2");
+    var c=eval("x - y");
+   var res=a + b + c;
+   console.log(res);
+}
+var newarray =number.map(myFunction)
+console.log(newarray)
+myFunction()
